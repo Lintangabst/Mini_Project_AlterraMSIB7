50486fcea66a6db26a9098d4b97a5f1b25304db8
Lintangabst <90893554+Lintangabst@users.noreply.github.com>
2024-11-18 16:31:19 +0700

    add vercel.json

--- client/app/Admin/page.tsx
+++ client/app/Admin/page.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import type { AppProps } from 'next/app';
 import AdminCrud from '../components/adminCrud';
-import PrelineScript from "../components/prelineScript";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
